e586c9bc2e570f342ed7192e8106a78ae768b997
Salma Hassan <salmahassan087@gmail.com>
2020-03-29 12:24:52 +0200

    Release Notes
    1] finish login & register screen
    2] add pod libs
    3] complate app flow

---
+++ SChat/Controllers/WelcmeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WelcmeViewController.swift
+//  SChat
+//
+//  Created by Salma Hassan on 3/29/20.
+//  Copyright © 2020 salma. All rights reserved.
+//
+
+import UIKit
+
+class WelcmeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SChat/Extensions/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  SChat
+//
+//  Created by Salma Hassan on 3/29/20.
+//  Copyright © 2020 salma. All rights reserved.
+//
+
+import Foundation
